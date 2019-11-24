63ca64d4abf61ce9216b4f92919d3ccce11ac0cf
akhil  em <akhilemcalicut@gmail.com>
2019-11-24 22:18:12 +0530

    changed alert

--- public/js/custom.js
+++ public/js/custom.js
@@ -5,7 +5,7 @@ function alertshow() {
 }
 
 function validate_user(form) {
-    alert("fhgjhgfjhgfjh");
+    alert("in validate methode");
     var name_len = form.name.value.length;
     var emailreg = /\S+@\S+\.\S+/;
     var passreg = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/;
@@ -16,7 +16,7 @@ function validate_user(form) {
     var cpass = form.Cpassword.value;
     var email = form.email.value;
     var pno = form.phone.value;
-
+        alert(dob+" "+pass+" "+cpass+" "+email+" "+pno);
     if (passreg.test(pass) == false) {
         alert("password must meet minimum requrements specified");
     } else if (pass != cpass) {
